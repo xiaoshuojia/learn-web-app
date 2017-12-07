e4ddbaa7a1e834e37210755e8e8f09267e7b7442
xiaoshuojia <xu_jie_2012@163.com>
2017-12-07 13:54:58 +0800

    first

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts');
 
 // var index = require('./routes/index');
 // var users = require('./routes/users');
@@ -25,6 +26,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressLayouts);    //使用页面布局模块
 // view的代码不是必需的，可以自己做出网页来，不必非要使用express的
 
 
